b05f85700850855094bdd2c45727aaf7256be093
Ivan <vanzaj@gmail.com>
2013-05-24 09:31:14 +0800

    added project skeleton

---
+++ dacets/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+#  _ \   \   __| __|__ __| __|
+#  |  | _ \ (    _|    | \__ \
+# ___/_/  _\___|___|  _| ____/
+
+"""
+DACETS - research data managment
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+__title__ = 'dacets'
+__version__ = '0.0.1'
+__author__ = 'Ivan Zimine'
+__license__ = 'Simplified BSD'
+__copyright__ = 'Copyright 2013 Ivan Zimine'
